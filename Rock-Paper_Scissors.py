0bfd100ff6329b461f3a377d98408fffc862a971
Manish <92672117+manish-xyz@users.noreply.github.com>
2022-06-04 20:06:46 +0530

    Update Rock-Paper_Scissors.py

--- Rock-Paper_Scissors.py
+++ Rock-Paper_Scissors.py
@@ -1,48 +1,46 @@
 import random
 
-choices = ["rock", "paper", "scissors"]
+while True:
+    choices = ["rock", "paper", "scissors"]
 
-computer = random.choice(choices)
-player = None
+    computer = random.choice(choices)
+    player = None
 
-while player not in choices:
-    player = (input("rock, paper, scissors? ")).lower()
+    while player not in choices:
+        player = (input("rock, paper, scissors? ")).lower()
 
-if player == computer:
-    print("Computer: ", computer)
-    print("Player: ", player)
-    print("Its a draw!")
-
-elif player == "rock":
-    if computer == "scissors" :
+    if player == computer:
         print("Computer: ", computer)
         print("Player: ", player)
-        print("Player win")
-    else :
-        print("Computer: ", computer)
-        print("Player: ", player)
-        print("Computer Win")
+        print("Its a draw!")
 
-elif player == "paper":
-    if computer == "rock" :
-        print("Computer: ", computer)
-        print("Player: ", player)
-        print("Player win")
-    else :
-        print("Computer: ", computer)
-        print("Player: ", player)
-        print("Computer Win")
+    elif player == "rock":
+        if computer == "scissors":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("Player win")
+        else:
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("Computer Win")
 
-elif player == "scissors":
-    if computer == "paper" :
-        print("Computer: ", computer)
-        print("Player: ", player)
-        print("Player win")
-    else :
-        print("Computer: ", computer)
-        print("Player: ", player)
-        print("Computer Win")
+    elif player == "paper":
+        if computer == "rock":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("Player win")
+        else:
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("Computer Win")
 
-
-
-
+    elif player == "scissors":
+        if computer == "paper":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("Player win")
+        else:
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("Computer Win")
+    print()
