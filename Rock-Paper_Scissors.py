822ae42a444b4b856b188003ea466f83b54c079c
manish-xyz <manishsharma43005@gmail.com>
2022-06-04 20:02:32 +0530

    added basic rock paper scissors program

---
+++ Rock-Paper_Scissors.py
@@ -0,0 +1,48 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+computer = random.choice(choices)
+player = None
+
+while player not in choices:
+    player = (input("rock, paper, scissors? ")).lower()
+
+if player == computer:
+    print("Computer: ", computer)
+    print("Player: ", player)
+    print("Its a draw!")
+
+elif player == "rock":
+    if computer == "scissors" :
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Player win")
+    else :
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Computer Win")
+
+elif player == "paper":
+    if computer == "rock" :
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Player win")
+    else :
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Computer Win")
+
+elif player == "scissors":
+    if computer == "paper" :
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Player win")
+    else :
+        print("Computer: ", computer)
+        print("Player: ", player)
+        print("Computer Win")
+
+
+
+
